004d97fc9a1eecbeca020c9eff56f46d656c3feb
solify123 <igorkuzmenko0412@gmail.com>
2025-10-17 00:36:30 +0200

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    port:5173,
     host: true,
     allowedHosts: ["overambitioned-confoundedly-jamila.ngrok-free.dev"]
   }
